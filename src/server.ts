8f52ec346d977886385df1b216dc30c74306246c
Ariel Hurtado <arielhurtado388@gmail.com>
2025-12-08 12:46:01 -0500

    Agregando CORS

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import router from "./routes/router";
 import { conexionDB } from "./config/db";
+import { corsConfig } from "./config/cors";
+
+conexionDB();
 
 const app = express();
 
-conexionDB();
+// Cors
+app.use(cors(corsConfig));
 
 // Leer datos de formularios
 app.use(express.json());
